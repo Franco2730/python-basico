e8cefa4ea41c077a7aebf9309525cb6d0cc7732c
franco2730 <franco.rosales2130@gmail.com>
2024-01-02 10:46:28 -0300

    Python-6

---
+++ L6/Directorio.py
@@ -0,0 +1,49 @@
+# Importa la clase Path de la biblioteca pathlib
+from pathlib import Path
+import os
+
+#getcwd traducido sería: obtener directorio de trabajo actual. Y hace justamente eso.
+ruta = os.getcwd() #c:/Users/54261/OneDrive/Escritorio/Carpetas-proyectos-semana/python-basico/L6/Directorio.py
+
+#chdir significa Change directori - Cambiar directorio.
+ruta2 = os.chdir(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6 - otro-directorio')
+
+archivo = open('Otro_archivo.txt')
+
+#print(archivo.read())
+
+# Ahora vamos a utilizar el metodo makedirs que, como su nombre lo indica, crea un directorio: con la siguiente linea se creara una nueva carpeta llamada MakeDirs
+# ruta3 = os.makedirs(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6\MakeDirs') 
+
+# A continuación vamos a eliminar un directorio con el metodo rmdir que significa: remove directori.
+# os.rmdir(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6\MakeDirs')
+
+#Ahora vamos a leer un archivo que se encuentre en otro directorio.
+# otro_archivo = open(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6 - otro-directorio\Otro_archivo.txt')
+# print(otro_archivo.read())
+
+# Ahora nos vamos a ayudar de la libreria pathlib 
+# Define una ruta de directorio utilizando la clase Path
+carpeta = Path(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6 - otro-directorio')
+# Combina la ruta del directorio con el nombre del archivo 'Otro_archivo.txt' para obtener la ruta completa del archivo
+archivo = carpeta / 'Otro_archivo.txt'
+
+mi_archivo = open(archivo)
+print(mi_archivo.read())
+
+# Finalmente, se debe cerrar el archivo después de usarlo para liberar los recursos
+mi_archivo.close()
+
+'''Una explicacion detallada de este último ejercicio:
+
+    from pathlib import Path: Importa la clase  Pathde la biblioteca pathlib, que proporciona una interfaz orientada a objetos para manipular rutas de archivos y directorios de manera más eficiente.
+
+    carpeta = Path(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6 - otro-directorio'): Crea un objeto Path llamado carpeta que representa la ruta del directorio especificado.
+
+    archivo = carpeta / 'Otro_archivo.txt': Combina la ruta del directorio (carpeta) con el nombre del archivo 'Otro_archivo.txt' utilizando el operador / proporcionado por pathlib. Esto crea un objeto Path que representa la ruta completa del archivo.
+
+    mi_archivo = open(archivo): Abre el archivo especificado en modo lectura ('r') y asigna el objeto de archivo resultante a la variable mi_archivo.
+
+    print(mi_archivo.read()): Imprime el contenido del archivo leído utilizando el método read() del objeto de archivo.
+
+    mi_archivo.close(): Es importante cerrar el archivo después de utilizarlo para liberar los recursos del sistema. En este caso, no está explícitamente cerrado en el código proporcionado, pero es una buena práctica cerrar el archivo manualmente después de su uso, o se puede utilizar un bloque with para asegurar que se cierre automáticamente cuando ya no sea necesario.'''
--- L6/ModificarTxt.py
+++ L6/ModificarTxt.py
@@ -9,3 +9,69 @@
 
     mi_archivo = open('archivo.txt','a') - Modo de solo escritura al final del archivo. Cuando utilizamos la 'a' como segundo parametro, se crea el archivo.txt (en caso de que no exista) y si ya existe, el cursor se posiciona al final del archivo para salvar los cambios que en el se alojen. No se borrará el contenido existente. 
     '''
+
+archivo = open(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6\prueba1.txt', 'w')
+
+archivo.write('Soy el nuevo texto\n') #De esta forma se va a crear un archivo llamado prueba1.txt con el contenido: 'Soy el nuevo texto'
+
+# ----
+
+archivo2 = open(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6\prueba.txt', 'w')
+
+archivo2.write('Creo que he borrado todo') #De esta forma se va a borrar todo lo que había en el archivo para escribir: 'Creo que he borrado todo'
+
+# ----
+
+archivo3 = open(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6\prueba1.txt', 'a')
+
+archivo3.write('''Soy 
+el nuevo 
+texto''') #De esta forma se va a crear un archivo llamado prueba1.txt con el contenido: 'Soy el nuevo texto' Para lograr un salto de linea se podria colocar \n o 3 comillas al principio y al fin como en el ejemplo.
+
+# ----
+
+#En el siguiente ejemplo, vamos a utilizar el metodo writelines, que vendria siendo como el metodo de la clase pasada: readlines, pero este es para escribir. No se utiliza mucho ya que, nosotros le tenemos que brindar un arreglo con str y dicho metodo concatenará todas las cadenas juntas.
+
+archivo4 = open(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6\prueba4.txt', 'w')
+archivo4.writelines(['Hola', 'mundo', 'here', 'i"m']) #Holamundoherei"m
+
+# ----
+
+#Como dijimos, la manera anterior no es muy utilizada, porque si quisieramos escribir un string por cada linea, el metodo writelines no nos ayuda, en su lugar, podriamos idear un codigo que por cada iteracion a cada elemento le agregue un salto de linea: 
+archivo5 = open(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6\prueba5.txt', 'w')
+lista = ['Hola', 'mundo', 'here', 'i"m'] 
+
+for palabra in lista: # FOR cada palabra IN lista...
+    archivo5.writelines(palabra + '\n')  # en el archivo abierto ARCHIVO5 vamos a escribir(cada palabra iterada del punto anterior mas un salto de linea)
+
+'''Hola
+mundo
+here
+i"m
+'''
+
+#----
+
+# A continuación, vamos a agregar una linea al archivo anterior, en vez de reescribir con 'w', simplemen te agregamos al final con 'a'
+
+archivo6 = open(r'C:\Users\54261\OneDrive\Escritorio\Carpetas-proyectos-semana\python-basico\L6\prueba5.txt', 'a')
+lista = ['Feliz 2024 !!'] 
+
+for palabra in lista: # FOR cada palabra IN lista...
+    archivo5.writelines(palabra + '\n')  # en el archivo abierto ARCHIVO5 vamos a escribir(cada palabra iterada del punto anterior mas un salto de linea)
+
+'''
+Hola
+mundo
+here
+i"m
+Feliz 2024 !! (Se agregó esta linea al final)
+'''
+
+
+archivo.close()
+archivo2.close()
+archivo3.close()
+archivo4.close()
+archivo5.close()
+
