2b95cc3b71d59942638c4a5d205655713357236a
franco2730 <franco.rosales2130@gmail.com>
2024-01-08 19:29:54 -0300

    Python-6

--- L6/Proyecto_recetas.py
+++ L6/Proyecto_recetas.py
@@ -99,62 +99,174 @@ def inicio():
     print('*' * 5 + " Bienvenido al administrador de recetas " + '*' * 5)
     print('*' * 50)
     print('\n')
-    print(f"Las recetas se encuentran en: {mi_ruta}")
-    print(f"Total recetas: {contar_recetas(mi_ruta)}")  # Acá le pasamos por parámetro a mi_ruta ya que pide un parámetro y nosotros necesitamos pasarle esa información.
+    print(f"Las recetas se encuentran en {mi_ruta}")
+    print(f"Total recetas: {contar_recetas(mi_ruta)}")
 
+    eleccion_menu = 'x'
+    # En el siguiente loop while decimos: MIENTRAS QUE NO sea un numero (isnumeric) eleccion_menu (lo que haya en esa variable) ( ) OR eleccion_menu (lo que haya en esa varuable) NO ESTE EN EL RANGO DEL (1 al 7) teniendo en cuenta que el segundo parametro no es inclucivo y será en este caso hasta el 6 entonces:...
 
-'''En el siguiente punto, debemos mostrarle al usuario todas las opciones y él tendrá que elegir una. Pero como dicho menú debe mostrarse una y otra vez mientras que el usuario no brinde una opción correcta vamos a hacerlo dentro de un loop WHILE'''
-
-eleccion_menu = 'x'
-# En el siguiente loop while decimos: MIENTRAS QUE NO sea un numero (isnumeric) eleccion_menu (lo que haya en esa variable) ( ) OR eleccion_menu (lo que haya en esa varuable) NO ESTE EN EL RANGO DEL (1 al 7) teniendo en cuenta que el segundo parametro no es inclucivo y será en este caso hasta el 6 entonces:...
-while not eleccion_menu.isnumeric() or int(eleccion_menu) not in range(1, 7):
-    print("Elige una opción: ")
-    print('''
-    [1] - Leer receta.
-    [2] - Crear receta nueva.
-    [3] - Crear nueva categoría.
-    [4] - Eliminar receta.
-    [5] - Eliminar categoría.
-    [6] - Salir del programa. ''')
+    '''En el siguiente punto, debemos mostrarle al usuario todas las opciones y él tendrá que elegir una. Pero como dicho menú debe mostrarse una y otra vez mientras que el usuario no brinde una opción correcta vamos a hacerlo dentro de un loop WHILE'''
     
-    eleccion_menu = input()
+    while not eleccion_menu.isnumeric() or int(eleccion_menu) not in range(1,7):
+        print("Elige una opcion:")
+        print('''
+        [1] - Leer receta
+        [2] - Crear receta nueva
+        [3] - Crear categoria nueva
+        [4] - Eliminar receta
+        [5] - Eliminar categoria
+        [6] - Salir del programa''')
+        eleccion_menu = input()
+
+    return int(eleccion_menu)
 
 # inicio()  # Puedes llamar a la función inicio() si deseas ejecutarla al final del script.
 
 
-menu = 0 # Acá se guardara la decision del cliente.
+# Ahora vamos a crear una funcion (una de las primeras) que sirva para mostrar las categorias:
+def mostrar_categorias(ruta):
+    print("Categorias:")
+    ruta_categorias = Path(ruta)
+    lista_categorias = []
+    contador = 1
 
-if menu == 1:
-    # Mostrar categorias
-    # Elegir categorias
-    # Mostrar recetas
-    # Elegir recetas
-    # Leer recetas
-    # Volver a inicio
-    pass
-elif menu == 2:
-    # Mostrar categorias
-    # Elegir categorias
-    # Crear receta nueva
-    # Volver a inicio
-    pass
-elif menu == 3:
-    # Crear categorias
-    # Volver a inicio
-    pass
-elif menu == 4:
-    # Mostrar categorias
-    # Elegir categorias
-    # Mostrar recetas
-    # Elegir recetas
-    # Eliminar recetas
-    # Volver a inicio
-    pass
-elif menu == 5:
-    # Mostrar categorias
-    # Elegir categorias
-    # Eliminar categorias
-    pass
-elif menu == 6:
-    # Finalizar programa.
-    pass
+    for carpeta in ruta_categorias.iterdir():
+        carpeta_str = str(carpeta.name)
+        print(f"[{contador}] - {carpeta_str}")
+        lista_categorias.append(carpeta)
+        contador += 1
+
+    return lista_categorias
+
+# Creando la funcion para mostrar las categorias:
+def elegir_categoria(lista):
+    eleccion_correcta = 'x'
+
+    while not eleccion_correcta.isnumeric() or int(eleccion_correcta) not in range(1, len(lista) + 1):
+        eleccion_correcta = input("\nElije una categoria: ")
+
+    return lista[int(eleccion_correcta) - 1]
+
+# Creando la fn para mostrar las recetas:
+def mostrar_recetas(ruta):
+    print("Recetas:")
+    ruta_recetas = Path(ruta)
+    lista_recetas = []
+    contador = 1
+
+    for receta in ruta_recetas.glob('*.txt'):
+        receta_str = str(receta.name)
+        print(f"[{contador}] - {receta_str}")
+        lista_recetas.append(receta)
+        contador += 1
+
+    return lista_recetas
+
+# Elegir una receta:
+def elegir_recetas(lista):
+    eleccion_receta = 'x'
+
+    while not eleccion_receta.isnumeric() or int(eleccion_receta) not in range(1, len(lista) + 1):
+        eleccion_receta = input("\nElije una receta: ")
+
+    return lista[int(eleccion_receta) - 1]
+
+# Leer una receta:
+def leer_receta(receta):
+    print(Path.read_text(receta))
+
+# Crear receta
+def crear_receta(ruta):
+    existe = False
+
+    while not existe:
+        print("Escribe el nombre de tu receta: ")
+        nombre_receta = input() + '.txt'
+        print("Escribe tu nueva receta: ")
+        contenido_receta = input()
+        ruta_nueva = Path(ruta, nombre_receta)
+
+        if not os.path.exists(ruta_nueva):
+            Path.write_text(ruta_nueva, contenido_receta)
+            print(f"Tu receta {nombre_receta} ha sido creada")
+            existe = True
+        else:
+            print("Lo siento, esa receta ya existe")
+
+#Crear categoria
+def crear_categoria(ruta):
+    existe = False
+
+    while not existe:
+        print("Escribe el nombre de la nueva categoria: ")
+        nombre_categoria = input()
+        ruta_nueva = Path(ruta, nombre_categoria)
+
+        if not os.path.exists(ruta_nueva):
+            Path.mkdir(ruta_nueva)
+            print(f"Tu nueva categoria {nombre_categoria} ha sido creada")
+            existe = True
+        else:
+            print("Lo siento Argento... esa categoria ya existe")
+
+# Eliminar receta:
+def eliminar_receta(receta):
+    Path(receta).unlink()   #unlink se utiliza para eliminar algo.
+    print(f"La receta {receta.name} ha sido eliminada con éxito")
+
+# Eliminar categoria
+def eliminar_categoria(categoria):
+    Path(categoria).rmdir() #rmdir se utiliza para eliminar un directorio.
+    print(f"La categoria {categoria.name} ha sido eliminada con éxito")
+
+# Fn para volver al inicio
+def volver_inicio():
+    eleccion_regresar = 'x'
+
+    while eleccion_regresar.lower() != 'v':
+        eleccion_regresar = input("\nPresione V para volver al menu: ")
+
+
+finalizar_programa = False
+
+while not finalizar_programa:
+    menu = inicio() # Acá se guardara la decision del cliente.
+
+    if menu == 1:
+        mis_categorias = mostrar_categorias(mi_ruta)
+        mi_categoria = elegir_categoria(mis_categorias)
+        mis_recetas = mostrar_recetas(mi_categoria)
+        if len(mis_recetas) < 1:
+            print("no hay recetas en esta categoría.")
+        else:
+            mi_receta = elegir_recetas(mis_recetas)
+            leer_receta(mi_receta)
+        leer_receta(mi_receta)
+        volver_inicio()
+
+    elif menu == 2:
+        mis_categorias = mostrar_categorias(mi_ruta)
+        mi_categoria = elegir_categoria(mis_categorias)
+        crear_receta(mi_categoria)
+        volver_inicio()
+
+    elif menu == 3:
+        crear_categoria(mi_ruta)
+        volver_inicio()
+
+    elif menu == 4:
+        mis_categorias = mostrar_categorias(mi_ruta)
+        mi_categoria = elegir_categoria(mis_categorias)
+        mis_recetas = mostrar_recetas(mi_categoria)
+        mi_receta = elegir_recetas(mis_recetas)
+        eliminar_receta(mi_receta)
+        volver_inicio()
+
+    elif menu == 5:
+        mis_categorias = mostrar_categorias(mi_ruta)
+        mi_categoria = elegir_categoria(mis_categorias)
+        eliminar_categoria(mi_categoria)
+
+    elif menu == 6:
+        finalizar_programa = True
+    
